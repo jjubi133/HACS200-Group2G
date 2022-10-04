41e9c82fadc154013d257196f1ef6e9186dcfac2
trevorlorin <114457931+trevorlorin@users.noreply.github.com>
2022-10-04 15:48:43 -0400

    Add files via upload

---
+++ logfileGrep.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#argument 1 will be the filename desired for the altered log and argument 2 will be the logfile
+touch $1
+
+echo ">>>ALL LINES INCLUDING WGET<<<\n" > $1
+grep "wget" $2 >> $1
+
+echo ">>>ALL LINES INCLUDING CURL<<<\n" > $1
+grep "curl" $2 >> $1
+
+echo ">>>ALL LINES INCLUDING INSTALL<<<\n" > $1
+grep "install" $2 > $1
+
+echo ">>>ALL LINES INCLUDING LS<<<\n" > $1
+grep "ls" $2 > $1
+
+echo ">>>ALL LINES INCLUDING CAT<<<\n" > $1
+grep "cat" $2 > $1
+
+ 
